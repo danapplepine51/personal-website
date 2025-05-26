12fa2368ddd9a8cb8d603c658f6a94964d2b2bcf
danapplepine51 <danapplepine51@gmail.com>
2025-05-26 18:06:03 -0400

    fix homepage and baseurl

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/personal-website/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
