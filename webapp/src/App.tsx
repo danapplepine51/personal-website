e6409834f1fd8f8925f67c1bfe14733d83bc5ea2
danapplepine51 <danapplepine51@gmail.com>
2025-05-26 15:23:41 -0400

    setup react ts and tailwindcss

---
+++ webapp/src/App.tsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+  return (
+    <div className="bg-red-500 w-full h-full">
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  )
+}
+
+export default App
